0c48d0a17b11a56658a68406a887215834b102ad
Jesse Nelson <jnels124@msudenver.edu>
2013-12-22 02:00:00 -0700

    init

---
+++ src/hang_man/dictionary.clj
@@ -0,0 +1,32 @@
+(ns hang-man.dictionary
+  (:require [clojure.java.io :as io]))
+
+(def max-word-length (int 29))
+(def min-word-length (int 2))
+(def defualt-dictionary "DefaultDictionary.txt")
+(def lowest-character (char \A))
+(def highest-character (char \z))
+;;(def dictionary '()) not needed probably, just return list dirctly 
+;; Not working yet
+;; 
+(defn read-in-dictionary 
+	"Reads in dictionary from file and returns data structure with contents"
+  [filename]
+  #_(slurp (str "resources/DefaultDictionary.txt"))
+  (with-open [rdr (io/reader "resources/DefaultDictionary.txt")]
+    (doseq [line (line-seq rdr)]
+      (println line))))
+
+;;Return random word with given size from the dictionary
+(defn randWord
+    "Returns a random word from the dictionary with the given word length"
+     [word-size] )
+
+(defn all-words-in-dictionary
+	"Returns all words in the dictionary with a given size"
+	[word-size]
+	)
+
+(defn new-dictionary-with-added-word
+	"Creates and returns new dictionary with the added word"
+	[word-size])
---
+++ test/hang_man/core_test.clj
@@ -0,0 +1,7 @@
+(ns hang-man.core-test
+  (:require [clojure.test :refer :all]
+            [hang-man.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
